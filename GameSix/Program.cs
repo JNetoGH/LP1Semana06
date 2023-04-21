4fab8eb860ac77b966f0761caeffe8d3f1919710
JNeto <joaomendes15@gmail.com>
2023-04-21 14:06:10 +0100

    Foes array initialized with user's input done

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace GameSix
+﻿using System;
+using System.Globalization;
+
+namespace GameSix
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            
+            int size = GetValidatedNumberFromUSer("Total of enemies: ", 
+                "Please insert a number bigger than 0", 
+                (i => i > 0 ));
+            Foe[] foes = new Foe[size];
+            for (int i = 0; i < foes.Length; i++)
+            {
+                Console.Write($"Please insert a name to Foe number {i+1}: ");
+                foes[i] = new Foe(Console.ReadLine());
+                
+            }
+            foreach (Foe foe in foes)
+                Console.WriteLine(foe.GetName());
         }
+
+        private static int GetValidatedNumberFromUSer(string inputMsg, string errorMsg, Predicate<int> validation)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write(inputMsg);
+                    int num = int.Parse(Console.ReadLine());
+                    if (!validation.Invoke(num))
+                        throw new Exception();
+                    return num;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(errorMsg+"\n");
+                }
+            }
+        }
+        
     }
 }
